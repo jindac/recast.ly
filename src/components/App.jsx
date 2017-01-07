b8ef801a55bced56171c42a27a0ef85717e03dce
jindac <dachanman91@gmail.com>
2017-01-06 19:20:40 -0800

    Implement VideoPlayer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,21 @@
-var App = (props) => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
-    </div>
-    <div className="col-md-5">
-      <VideoList videos={props.videos}/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer video={this.props.videos[0]}/>
+        </div>
+        <div className="col-md-5">
+          <VideoList videos={this.props.videos}/>
+        </div>
+      </div>
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // TODO: Render the `App` component to the DOM
-console.log(App);
-console.log(window.exampleVideoData);
+console.log('App', App);
+console.log('window', window.exampleVideoData);
 ReactDOM.render(<App videos = {window.exampleVideoData} />, document.getElementById('app'));
 console.log('hell yes again again again');
