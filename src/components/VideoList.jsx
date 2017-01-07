70308b2bd7c321b20f888c42a5716e6eddfccba1
jindac <dachanman91@gmail.com>
2017-01-06 16:28:40 -0800

    Implement videoList

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,14 +1,7 @@
-var VideoList = () => (
+var VideoList = (props) => (
+
   <div className="video-list media">
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
+    {props.videos.map((video) => <VideoListEntry video = {video} />)}
   </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 // TODO: Render the `App` component to the DOM
-
+console.log(App);
+ReactDOM.render(<App />, document.getElementById('app'));
+console.log('hell yes again again');
